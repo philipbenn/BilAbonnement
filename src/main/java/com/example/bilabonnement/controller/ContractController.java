5951e3ecf3254f78ddc85e50f639ebcfed7eb13a
NoahWederking <noah376b@stud.kea.dk>
2023-05-16 09:23:35 +0200

    contract view irrelevant now ;(

--- src/main/java/com/example/bilabonnement/controller/ContractController.java
+++ src/main/java/com/example/bilabonnement/controller/ContractController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ContractController {
@@ -17,4 +18,12 @@ public class ContractController {
         model.addAttribute( "getContractInfo", contractService.getAllContractInfo());
         return "/contracts";
     }
+
+    @GetMapping("/addContractForm/{customer_id}")
+    public String addContractForm(@PathVariable int customer_id, Model model){
+        model.addAttribute("customer_id", customer_id);
+        return "/addContractForm";
+    }
+
+    //TODO postmapping(add contract) ReqeustParam customer_id, de andre felter  request Param
 }
