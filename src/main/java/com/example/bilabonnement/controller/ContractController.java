d032ccfa690ca7490dcd437020b3e52f9463683f
oliv140 <oliv140x@stud.kea.dk>
2023-05-18 09:41:36 +0200

    Changes by louis

--- src/main/java/com/example/bilabonnement/controller/ContractController.java
+++ src/main/java/com/example/bilabonnement/controller/ContractController.java
@@ -78,7 +78,7 @@ public class ContractController {
     @PostMapping("/editSpecificContract")
     public String editSpecificContract(@RequestParam String contract_start_date, @RequestParam String contract_end_date, @RequestParam int contract_id){
         contractService.updateStartAndEndDate(contract_id, contract_start_date, contract_end_date);
-        return ("contract/contractsOverview");
+        return ("redirect:/editContract/" + contract_id);
     }
 
 
